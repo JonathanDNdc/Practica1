08eed652b52d62e879eb127c6678507034ca22b8
JonathanDNdc <35204824+JonathanDNdc@users.noreply.github.com>
2020-02-18 22:26:24 -0600

    Update adder.vhdl

--- FullAdder/adder.vhdl
+++ FullAdder/adder.vhdl
@@ -1,10 +1,15 @@
 entity adder is
+  -- `i0`, `i1`, y el accarreo de entrada `ci` son entradas del sumador.
+  -- `s` es la salida de suma, `co` es el accarreo de salida.
     port(i0, i1, ci : in bit;
         s, co : out bit);
 end adder;
 
 architecture rtl of adder is
 begin
-    s  <= i0 xor i1 xor ci;
+  --  La arquitectura de este sumador completo contiene dos asignaciones concurrentes.
+  --  Calcula la suma.
+  s <= i0 xor i1 xor ci;
+  --  Calcula el acarreo.
     co <= (i0 and i1) or (i0 and ci) or (i1 and ci);
 end rtl;
